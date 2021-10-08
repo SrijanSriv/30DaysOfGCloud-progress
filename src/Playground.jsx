824b5e8bb1d1887518bd3ea9011729fed4ad8655
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-10-08 14:30:43 +0530

    sorted the list

--- src/Playground.jsx
+++ src/Playground.jsx
@@ -3,6 +3,17 @@ import Badge from './Badge';
 import list from './list.json';
 
 const Playground = () => {
+
+    function sortResults(prop1, prop2, asc) {
+    list.sort(function(a, b) {
+        if (asc) {
+            return (a[prop1] + a[prop2]> b[prop1]+b[prop2]) ? 1 : ((a[prop1] + a[prop2]< b[prop1]+b[prop2]) ? -1 : 0);
+        } else {
+            return (a[prop1] + a[prop2]< b[prop1]+b[prop2]) ? 1 : ((a[prop1] + a[prop2]> b[prop1]+b[prop2]) ? -1 : 0);
+        }
+    });
+    }
+    sortResults('# of Skill Badges Completed in Track 1', '# of Skill Badges Completed in Track 2' , false);
     return(
         <div id = "playground">
             {list.map((val,index) => {
